263dafa2c775d9cd06633ac982625373ca9c8e72
Pedro J. Fernandez <esquillidev@gmail.com>
2019-05-04 01:24:25 -0400

    Redesigned packages HTML, CSS, and JS

---
+++ packages.js
@@ -0,0 +1,31 @@
+document.getElementById("Lisa").addEventListener("click", packagesEventHandler);
+document.getElementById("GetLisa").addEventListener("click", packagesEventHandler);
+
+document.getElementById("FakeDisabled").addEventListener("click", packagesEventHandler);
+document.getElementById("GetFakeDisabled").addEventListener("click", packagesEventHandler);
+
+document.getElementById("Screender").addEventListener("click", packagesEventHandler);
+document.getElementById("GetScreender").addEventListener("click", packagesEventHandler);
+
+function packagesEventHandler(e) {
+  switch(e.currentTarget.id) {
+    case "Lisa":
+      window.open("https://esquillidev.github.io/description.html?id=com.esquilli.lisa", "_self");
+      break;
+    case "GetLisa":
+      break;
+    case "FakeDisabled":
+      window.open("https://esquillidev.github.io/description.html?id=com.esquilli.fakedisabled", "_self");
+      break;
+    case "GetFakeDisabled":
+      window.open("cydia://url/https://cydia.saurik.com/api/share#?source=https://esquillidev.github.io/&amp;package=com.esquilli.fakedisabled");
+      break;
+    case "Screender":
+      window.open("https://esquillidev.github.io/description.html?id=com.esquilli.screender", "_self");
+      break;
+    case "GetScreender":
+      window.open("cydia://url/https://cydia.saurik.com/api/share#?source=https://esquillidev.github.io/&amp;package=com.esquilli.screender");
+      break;  
+  }
+  e.stopPropagation();
+}
